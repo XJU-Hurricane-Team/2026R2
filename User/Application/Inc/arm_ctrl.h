32952d15375c7aec6eb9e731805ec401e69899e3
xinglu <1685820272@qq.com>
2026-07-08 23:09:01 +0800

    全流程竞技赛

--- User/Application/Inc/arm_ctrl.h
+++ User/Application/Inc/arm_ctrl.h
@@ -13,7 +13,7 @@
 
 /* ================== 应用层宏定义 ================== */
 #define ARM_USE_REMOTE_KEY                                                     \
-    1 /**< 是否使用遥控器按键切换状态位 (0:禁用, 1:启用) */
+    0 /**< 是否使用遥控器按键切换状态位 (0:禁用, 1:启用) */
 #define ARM_TASK_PERIOD_MS      10    /**< 机械臂控制任务周期 (毫秒) */
 #define ARM_REACH_POS_TOL_MM    5.0f  /**< 位置到位判定容差 (mm) */
 #define ARM_REACH_JOINT_TOL_RAD 0.10f /**< 关节角到位判定容差 (rad) */
